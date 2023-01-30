457b1a0704ce82307791e81e1ae40d3c56794e01
Harrison Boateng <odameharrison13@gmail.com>
2023-01-30 20:36:42 +0000

    done

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import users_reducers from "./reducers/users_reducers";
+import teacher_reducer from "./reducers/teacher_reducer";
 
 export default configureStore({
-  reducer: { usersReducer: users_reducers },
+  reducer: 
+  {
+     students: users_reducers,
+     teachers: teacher_reducer
+    },
 });
-
+ 
 
